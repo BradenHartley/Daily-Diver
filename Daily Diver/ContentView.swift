4e9cf3e67126ee2e0b2deedcc9f0086248ffdeca
Braden Hartley <Bradenhartley@hotmail.com>
2023-06-16 16:54:20 -0600

    Initial Commit

---
+++ Daily Diver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Daily Diver
+//
+//  Created by Braden Hartley on 6/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Daily Diver/Daily_DiverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_DiverApp.swift
+//  Daily Diver
+//
+//  Created by Braden Hartley on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Daily_DiverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
